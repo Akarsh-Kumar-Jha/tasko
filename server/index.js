1e5608d495383c7a4f571b69a317bf603a830ae5
AKARSH KUMAR <160502447+Akarsh-Kumar-Jha@users.noreply.github.com>
2025-04-11 23:49:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,22 +12,27 @@ dbConnection();
 app.use(express.json());
 app.use(cookieParser());
 
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://tasko-frontend-p3y7.onrender.com',
-];
-
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://tasko-frontend-p3y7.onrender.com',
+    ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
 
 app.use('/api/v1', taskRoutes);
 app.use('/api/v1/user',userRoutes);
